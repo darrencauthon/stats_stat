d30b0862040602bcecb6e61198d3c50bc9d547b2
Darren Cauthon <darren@cauthon.com>
2013-05-27 16:02:06 -0500

    Jumped ahead, step back.

--- lib/stats_stat/tracker.rb
+++ lib/stats_stat/tracker.rb
@@ -4,8 +4,8 @@ module StatsStat
       @data_source = data_source
     end
     
-    def track(the_event)
-      @data_source.record_event(the_event)
+    def track the_event
+      @data_source.record_event :the_special_event
     end
   end
 end
