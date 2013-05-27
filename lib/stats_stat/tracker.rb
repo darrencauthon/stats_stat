54343018c1e760e3c4e58b1439eb40363cd81082
Caleb Cauthon <calebcauthon@gmail.com>
2013-05-27 11:33:00 -0500

    added the track method

--- lib/stats_stat/tracker.rb
+++ lib/stats_stat/tracker.rb
@@ -1,4 +1,11 @@
 module StatsStat
   class Tracker
+    def initialize(data_source)
+      @data_source = data_source
+    end
+    
+    def track(the_event)
+      @data_source.record_event(the_event)
+    end
   end
 end
