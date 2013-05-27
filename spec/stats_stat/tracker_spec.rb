b05da5ee33faceeda5bd934599aa723930b4e3db
Darren Cauthon <darren@cauthon.com>
2013-05-27 16:16:16 -0500

    Another failing test.

--- spec/stats_stat/tracker_spec.rb
+++ spec/stats_stat/tracker_spec.rb
@@ -4,22 +4,22 @@ describe StatsStat::Tracker do
 
   let(:tracker) { StatsStat::Tracker.new data_source }
 
-  describe "with a valid data source" do
+  [:the_special_event, :another_event].each do |the_event|
+    describe "with a valid data source" do
 
-    let(:data_source) { mock() }
+      let(:data_source) { mock() }
 
-    describe "tracking an event" do
+      describe "tracking an event" do
 
-      let(:the_event) { :the_special_event }
+        before do
+          data_source.expects(:record_event).with(the_event)
 
-      before do
-        data_source.expects(:record_event).with(the_event)
+          tracker.track the_event
+        end 
 
-        tracker.track the_event
-      end 
-
-      it "should send the event to the data source" do
-        # assertion set in mock above
+        it "should send the event to the data source" do
+          # assertion set in mock above
+        end
       end
     end
   end
