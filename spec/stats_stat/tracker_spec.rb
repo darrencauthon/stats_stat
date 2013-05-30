512e455312f59007507d554a9a37aa2fa56c6456
Darren Cauthon <darren@cauthon.com>
2013-05-29 22:28:22 -0500

    Refactor the specs.

--- spec/stats_stat/tracker_spec.rb
+++ spec/stats_stat/tracker_spec.rb
@@ -4,28 +4,31 @@ describe StatsStat::Tracker do
 
   let(:tracker) { StatsStat::Tracker.new data_source }
 
-  [:event, :now].to_objects {[
-    [:the_special_event, Time.parse('2013/1/4')],
-    [:another_event,     Time.parse('2013/1/4')],
-    [:another_event,     Time.parse('2014/2/3')]
-  ]}.each do |test|
-    describe "with a valid data source" do
+  describe 'track' do
+    [:event, :now].to_objects {[
+      [:the_special_event, Time.parse('2013/1/4')],
+      [:another_event,     Time.parse('2013/1/4')],
+      [:another_event,     Time.parse('2014/2/3')]
+    ]}.each do |test|
+      describe "with a valid data source, but no data" do
 
-      let(:data_source) { mock() }
+        let(:data_source) { mock() }
 
-      describe "tracking an event" do
+        describe "tracking an event" do
 
-        before do
-          Timecop.freeze test.now
-          data_source.expects(:record_event).with(test.event, { created_at: test.now } )
+          before do
+            Timecop.freeze test.now
+            data_source.expects(:record_event).
+              with(test.event, { created_at: test.now } )
 
-          tracker.track test.event
-        end 
+            tracker.track test.event
+          end 
 
-        after { Timecop.return }
+          after { Timecop.return }
 
-        it "should send the event to the data source" do
-          # assertion set in mock above
+          it "should send the event to the data source" do
+            # assertion set in mock above
+          end
         end
       end
     end
