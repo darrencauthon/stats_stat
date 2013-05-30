e32f19f93ddbec1f3ec1fc57781036bf837cfcd8
Darren Cauthon <darren@cauthon.com>
2013-05-29 22:31:56 -0500

    Try passing some data.

--- spec/stats_stat/tracker_spec.rb
+++ spec/stats_stat/tracker_spec.rb
@@ -32,5 +32,31 @@ describe StatsStat::Tracker do
         end
       end
     end
+
+    [:event, :now, :data].to_objects {[
+      [:the_special_event, Time.parse('2013/1/4'), { some: 'data' } ]
+    ]}.each do |test|
+      describe "with a valid data source, but no data" do
+
+        let(:data_source) { mock() }
+
+        describe "tracking an event" do
+
+          before do
+            Timecop.freeze test.now
+            data_source.expects(:record_event).
+              with(test.event, { created_at: test.now, data: test.data } )
+
+            tracker.track test.event, test.data
+          end 
+
+          after { Timecop.return }
+
+          it "should send the event to the data source" do
+            # assertion set in mock above
+          end
+        end
+      end
+    end
   end
 end
