8213ca8a73a42c8a4839239a59ab163af1e0d60f
Darren Cauthon <darren@cauthon.com>
2013-05-29 22:17:23 -0500

    Set up the next failing test.

--- spec/stats_stat/tracker_spec.rb
+++ spec/stats_stat/tracker_spec.rb
@@ -6,7 +6,8 @@ describe StatsStat::Tracker do
 
   [:event, :now].to_objects {[
     [:the_special_event, Time.parse('2013/1/4')],
-    [:another_event,     Time.parse('2013/1/4')]
+    [:another_event,     Time.parse('2013/1/4')],
+    [:another_event,     Time.parse('2014/2/3')]
   ]}.each do |test|
     describe "with a valid data source" do
 
@@ -15,10 +16,13 @@ describe StatsStat::Tracker do
       describe "tracking an event" do
 
         before do
+          Timecop.freeze test.now
           data_source.expects(:record_event).with(test.event, { created_at: test.now } )
 
           tracker.track test.event
         end 
+
+        after { Timecop.return }
 
         it "should send the event to the data source" do
           # assertion set in mock above
